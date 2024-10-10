66526ae17e8f5f3c099de1d19cdb4f14d635870e
krthk-22 <cs22b026@smail.iitm.ac.in>
2024-10-11 01:04:45 +0530

    Added hull_images

--- Assignments/Assignment3/run.sh
+++ Assignments/Assignment3/run.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 g++ -o convex_hull incremental_3d.cpp
 
-for i in {4..10}
+for i in {4..100}
 do
     ./convex_hull point_sets/point_set$i.txt hull_files/convex_hull$i.stl
     python3 visualisation.py $i save
